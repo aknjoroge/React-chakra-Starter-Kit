8d9a85949e42e318af73452f66c1d7ecdb7237a6
aknjoroge <alexkaranja629@gmail.com>
2022-05-20 16:49:14 +0300

    downgraded framer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>,
+  document.getElementById("root")
+);
